7e730c878c43980c58f9de0b163aff5cf07cef07
Salmna <fuqrayy@gmail.com>
2016-09-29 20:44:53 +0500

    Added the info button and reverse geoccoding

--- web/maps/url.py
+++ web/maps/url.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
     url(r'^getFeatureInfo/$', views.feature_info, name = 'featureInfo'),
     url(r'^getPointInfo/$', views.point_info, name = 'pointInfo'),
-    url(r'^getPointDistance/$', views.calculate_distance, name = 'point_distance')
+    url(r'^getPointDistance/$', views.calculate_distance, name = 'point_distance'),
+    url(r'^check-status/$', views.check_status, name = 'check-status'),
 ]
--- web/maps/views.py
+++ web/maps/views.py
@@ -10,41 +10,6 @@ import requests, json
 # Create your views here.
 
 def index(request):
-    # lat = -8.589420318603516
-    # lon = 52.67669432297103
-    # data = {}
-    # query = "SELECT gid, id, name, end_date, flood_reco  FROM flood_polys where ST_Contains(ST_Transform(geom, 4326), ST_GeomFromText('POINT(%f %f)', 4326));" %(lat, lon)
-    # for x in FloodPolys.objects.raw(query):
-    #     if x.name:
-    #         data = {'id': x.id, 'name': x.name, 'end_date': x.end_date, 'flood_record': x.flood_reco}
-    # if not data:
-    #     print 'empty'
-    # else:
-    #     print json.dumps(data, default=json_serial)
-    # p = "WGS 84"
-    # lat = -8.631606101989746
-    # lon = 52.67340916376591
-    # data = {}
-    # danger = []
-    # medium = []
-    # low = []
-
-    # query_danger = "SELECT gid, ST_Distance_Spheroid(ST_Transform(geom, 4326), ST_GeomFromText('POINT(%f %f)', 4326),'SPHEROID[\"WGS 84\", 6378137, 298.257223563]') AS dis FROM fluvial_10 ORDER BY dis ASC;" %(lat, lon)
-    # query_medium = "SELECT gid, ST_Distance_Spheroid(ST_Transform(ST_Boundary(geom), 4326), ST_GeomFromText('POINT(%f %f)', 4326),'SPHEROID[\"WGS 84\", 6378137, 298.257223563]') AS dis FROM fluvial_1 ORDER BY dis ASC;" %(lat, lon)
-    # query_low = "SELECT gid, ST_Distance_Spheroid(ST_Transform(ST_Boundary(geom), 4326), ST_GeomFromText('POINT(%f %f)', 4326),'SPHEROID[\"WGS 84\", 6378137, 298.257223563]') AS dis FROM fluvial_0_1 ORDER BY dis ASC;" %(lat, lon)
-
-    # for dngr, med, lw in zip(FloodPolys.objects.raw(query_danger), FloodPolys.objects.raw(query_medium), FloodPolys.objects.raw(query_low)):
-    #     if dngr.gid:
-    #         danger.append(dngr.dis)
-    #     if med.gid:
-    #         medium.append(med.dis)
-    #     if lw.gid:
-    #         low.append(lw.dis)
-    # data.update({'danger':danger[0], 'medium': medium[0], 'low':low[0]})
-    # if not data:
-    #     print 'empty'
-    # else:
-    #     print json.dumps(data, default=json_serial)
     return render(request, 'maps/index.html')
 
 def feature_info(request, *arg, **kargs):
@@ -102,6 +67,43 @@ def point_info(request):
             print 'empty'
         else:
             print json.dumps(data, default=json_serial)
+        return HttpResponse(json.dumps(data, default=json_serial))
+
+def check_status(request):
+    # lat = -8.589420318603516
+    # lon = 52.67669432297103
+    # data = {}
+    # query = "SELECT gid, id, name, end_date, flood_reco  FROM flood_polys where ST_Contains(ST_Transform(geom, 4326), ST_GeomFromText('POINT(%f %f)', 4326));" %(lat, lon)
+    # for x in FloodPolys.objects.raw(query):
+    #     if x.name:
+    #         data = {'id': x.id, 'name': x.name, 'end_date': x.end_date, 'flood_record': x.flood_reco}
+    # if not data:
+    #     print 'empty'
+    # else:
+    #     print json.dumps(data, default=json_serial)
+
+    lat = -8.631606101989746
+    lon = 52.67340916376591
+    data = {}
+    danger = []
+    medium = []
+    low = []
+
+    query_danger = "SELECT gid, ST_Distance_Spheroid(ST_Transform(geom, 4326), ST_GeomFromText('POINT(%f %f)', 4326),'SPHEROID[\"WGS 84\", 6378137, 298.257223563]') AS dis FROM fluvial_10 ORDER BY dis ASC;" %(lat, lon)
+    query_medium = "SELECT gid, ST_Distance_Spheroid(ST_Transform(ST_Boundary(geom), 4326), ST_GeomFromText('POINT(%f %f)', 4326),'SPHEROID[\"WGS 84\", 6378137, 298.257223563]') AS dis FROM fluvial_1 ORDER BY dis ASC;" %(lat, lon)
+    query_low = "SELECT gid, ST_Distance_Spheroid(ST_Transform(ST_Boundary(geom), 4326), ST_GeomFromText('POINT(%f %f)', 4326),'SPHEROID[\"WGS 84\", 6378137, 298.257223563]') AS dis FROM fluvial_0_1 ORDER BY dis ASC;" %(lat, lon)
+
+    for dngr, med, lw in zip(FloodPolys.objects.raw(query_danger), FloodPolys.objects.raw(query_medium), FloodPolys.objects.raw(query_low)):
+        if dngr.gid:
+            danger.append(dngr.dis)
+        if med.gid:
+            medium.append(med.dis)
+        if lw.gid:
+            low.append(lw.dis)
+    data.update({'danger':danger[0], 'medium': medium[0], 'low':low[0]})
+    if not data:
+        return HttpResponse('empty')
+    else:
         return HttpResponse(json.dumps(data, default=json_serial))
 
 def json_serial(obj):
